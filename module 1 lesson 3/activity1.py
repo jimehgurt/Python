aa21154826f135f75318e7f448d1b0b96f3b4bba
jimehgurt <zain.100.12l@gmail.com>
2025-07-26 17:40:40 +0500

    activity 3

--- module 1 lesson 3/activity1.py
+++ module 1 lesson 3/activity1.py
@@ -16,3 +16,11 @@ a = 150
 print ("this is the original data type of a" , type(a))
 a = str(a)
 print ("this is the changed data type of a" , type(a))
+
+
+F = "CoDiNgAl"
+print (len(F))
+print (F.upper())
+print (F.lower())
+reverse = F[::-1]
+print (reverse)
