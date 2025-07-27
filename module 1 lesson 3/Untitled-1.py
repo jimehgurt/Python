bd3e35d88036adf0b569c00a05cbb0edbd80aa21
jimehgurt <zain.100.12l@gmail.com>
2025-07-27 15:53:17 +0500

    project 2

---
+++ module 1 lesson 3/Untitled-1.py
@@ -0,0 +1,11 @@
+birthday_1 = "january 10k, 2010"
+birthday_2 = "march 22, 2012"
+birthday_3 = "july 5, 2008"
+birthday_4 = "october 30, 2011"
+birthday_5 = "december 15, 2009"
+
+print("birthday 1:", birthday_1)
+print("birthday 2:", birthday_2)
+print("birthday 3:", birthday_3)
+print("birthday 4:", birthday_4)
+print("birthday 5:", birthday_5)
